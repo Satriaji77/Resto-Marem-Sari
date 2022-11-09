e411660544d48159084d148a80517a997be14b63
Syahrindra Satriaji <106364023+Satriaji77@users.noreply.github.com>
2022-11-09 10:04:48 +0700

    Update db_marem-sari.sql

--- db_marem-sari.sql
+++ db_marem-sari.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pwl_kasir_restoran`
+-- Database: `db_marem-sari`
 --
 
 -- --------------------------------------------------------
